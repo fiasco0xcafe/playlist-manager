0a6343a4df86d662416cf15a61c49ea62a0d052a
fiasco0xcafe <129623758+fiasco0xcafe@users.noreply.github.com>
2025-09-19 03:42:16 -0400

    updated main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
+void add_song(char *song_choice, size_t song);
+
 int main(int argc, char **argv) {
+    char song[128];
+
     if (argc < 2) {
         fprintf(stderr, "Please enter an option\n");
         return 1;
@@ -9,6 +13,14 @@ int main(int argc, char **argv) {
     else if (strcmp(argv[1], "--help") == 0) {
         puts("usage: ./playlist [command]");
     }
+    else if (strcmp(argv[1], "add") == 0){
+        add_song(song, sizeof(song));
+    }
         
     return 0;
+}
+
+void add_song(char *song_choice, size_t song) {
+    printf("Enter a song: ");
+    fgets(song_choice, song, stdin);
 }
